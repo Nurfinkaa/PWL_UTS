8c0b3556617be6ab3ed37aeaf53581ae44938595
Nurfinka <nurfinka.lailasarii@gmail.com>
2026-04-21 09:07:27 +0700

    membuat CRUD barang

---
+++ app/Filament/Resources/Barangs/Schemas/BarangForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Resources\Barangs\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\Select;
+
+class BarangForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('kategori_id')
+                    ->required()
+                    ->numeric(),
+                Select::make('kategori_id')
+                    ->label('Kategori')
+                    ->relationship('kategori', 'kategori_nama')
+                    ->required(),
+                TextInput::make('barang_kode')
+                    ->required(),
+                TextInput::make('barang_nama')
+                    ->required(),
+                TextInput::make('harga_beli')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('harga_jual')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+}
