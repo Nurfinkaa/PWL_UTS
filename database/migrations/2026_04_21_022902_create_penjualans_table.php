90b9f3c318a98af994506ef75587fe155e0b51b7
Nurfinka <nurfinka.lailasarii@gmail.com>
2026-04-21 09:30:38 +0700

    membuat model dan migration penjualan

---
+++ database/migrations/2026_04_21_022902_create_penjualans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_penjualan', function (Blueprint $table) {
+            $table->id('penjualan_id');
+            $table->foreignId('user_id')
+                ->constrained('m_user', 'user_id')
+                ->onDelete('cascade');
+            $table->date('tanggal');
+            $table->integer('total_harga')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penjualans');
+    }
+};
