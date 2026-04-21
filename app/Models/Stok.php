4765b05e2ceba0f592c6f8a0381d2a1416491007
Nurfinka <nurfinka.lailasarii@gmail.com>
2026-04-21 09:26:46 +0700

    memperbaiki form stok dengan relasi barang, supplier, dan user

--- app/Models/Stok.php
+++ app/Models/Stok.php
@@ -17,4 +17,19 @@ class Stok extends Model
         'tanggal',
         'jumlah',
     ];
+
+    public function barang()
+    {
+        return $this->belongsTo(Barang::class, 'barang_id', 'barang_id');
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class, 'supplier_id', 'supplier_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(MUser::class, 'user_id', 'user_id');
+    }
 }
