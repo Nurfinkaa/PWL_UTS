3d6acb23737afc1d931cd7408cd45c1921fd6de8
Nurfinka <nurfinka.lailasarii@gmail.com>
2026-04-21 08:58:25 +0700

    membuat model dan migration barang

---
+++ app/Models/Barang.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Barang extends Model
+{
+    //
+    protected $table = 'm_barang';
+    protected $primaryKey = 'barang_id';
+
+    protected $fillable = [
+        'kategori_id',
+        'barang_kode',
+        'barang_nama',
+        'harga_beli',
+        'harga_jual',
+    ];
+
+}
