94b778f2a5f637e87a460f27ef72bff436a7a7da
Nurfinka <nurfinka.lailasarii@gmail.com>
2026-04-21 08:43:40 +0700

    membuat model dan migration supplier

---
+++ app/Models/Supplier.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Supplier extends Model
+{
+    //
+    protected $table = 'm_supplier';
+    protected $primaryKey = 'supplier_id';
+     protected $fillable = [
+        'supplier_kode',
+        'supplier_nama',
+        'supplier_alamat',
+     ];
+     public $timestamps = false;
+}
