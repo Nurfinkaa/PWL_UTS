6e445c2e9714e226190bd10d361a136f55954cd3
Nurfinka <nurfinka.lailasarii@gmail.com>
2026-04-21 22:57:04 +0700

    update file

--- app/Filament/Resources/Kategoris/KategoriResource.php
+++ app/Filament/Resources/Kategoris/KategoriResource.php
@@ -16,7 +16,7 @@ use Filament\Tables\Table;
 
 class KategoriResource extends Resource
 {
-    protected static ?string $navigationLabel = 'Kategori';
+    //protected static ?string $navigationLabel = 'Kategori';
 
     protected static ?string $model = Kategori::class;
 
